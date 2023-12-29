488636dca2b3541ed42eff599590319e69ca206d
salastro <salah2112004@gmail.com>
2023-12-29 17:27:46 +0200

    Add ifmain for matrix_inverse.py

--- src/etd_la/matrix_inverse.py
+++ src/etd_la/matrix_inverse.py
@@ -25,3 +25,18 @@ def calculate_inverse_solution(matrix_M, vector_B):
     solution_vector_m = np.dot(inverse_matrix, vector_B)
 
     return solution_vector_m
+
+if __name__ == '__main__':
+    # Define the coefficient matrix M
+    matrix_M = np.array([[0.2, 0.3, 0.5],
+                         [0.4, 0.3, 0.3],
+                         [0.4, 0.4, 0.2]])
+
+    # Define the constant vector B
+    vector_B = np.array([1, 1, 1])
+
+    # Calculate the solution vector m
+    solution_vector_m = calculate_inverse_solution(matrix_M, vector_B)
+
+    # Print the solution vector m
+    print(solution_vector_m)
