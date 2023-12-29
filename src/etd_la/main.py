27022ec09c60d2a15f5a88a500f5ce1a4eedad75
salastro <salah2112004@gmail.com>
2023-12-29 19:46:36 +0200

    Implement command line argument parsing

--- src/etd_la/main.py
+++ src/etd_la/main.py
@@ -1,26 +1,45 @@
+import argparse
+
 from plotting import plot_temperature_distribution
 from temperature_solver import solve_temperature_equation
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Solve and plot temperature distribution."
+    )
+    parser.add_argument("grid_size", type=int, help="Grid size for the simulation")
+    parser.add_argument(
+        "left_boundary_temp", type=float, help="Temperature at the left boundary"
+    )
+    parser.add_argument(
+        "up_boundary_temp", type=float, help="Temperature at the upper boundary"
+    )
+    parser.add_argument(
+        "right_boundary_temp", type=float, help="Temperature at the right boundary"
+    )
+    parser.add_argument(
+        "down_boundary_temp", type=float, help="Temperature at the lower boundary"
+    )
+
+    return parser.parse_args()
+
+
 def main():
-    # Problem parameters
-    grid_size = 10
-    left_boundary_temp = 100
-    up_boundary_temp = 0
-    right_boundary_temp = 0
-    down_boundary_temp = 0
+    # Parse command line arguments
+    args = parse_arguments()
 
     # Solve the temperature equation
     temperature_solution = solve_temperature_equation(
-        grid_size,
-        left_boundary_temp,
-        up_boundary_temp,
-        right_boundary_temp,
-        down_boundary_temp,
+        args.grid_size,
+        args.left_boundary_temp,
+        args.up_boundary_temp,
+        args.right_boundary_temp,
+        args.down_boundary_temp,
     )
 
     # Plot the temperature distribution
-    plot_temperature_distribution(grid_size, temperature_solution)
+    plot_temperature_distribution(args.grid_size, temperature_solution)
 
 
 if __name__ == "__main__":
